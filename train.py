fc27bd525929b12ac2e3304d028c5d6e58d778ca
Daniil Davydov <danyadavydov@yandex.ru>
2025-02-07 21:41:06 +0300

    [+] add model saving.

--- train.py
+++ train.py
@@ -1,20 +1,21 @@
 import keras
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
+from datetime import date
+import pickle
+import os
 
 import model as Model
 
 
 def train_model(train_dataset, epochs=10, batch_size=32, learning_rate=0.001):
-    # Компиляция модели
     model = Model.create_model()
     model.compile(
         optimizer=Adam(learning_rate=learning_rate),
         loss=keras.losses.MeanSquaredError(),
-        metrics=[keras.metrics.R2Score]
+        metrics=[keras.metrics.R2Score()]
     )
 
-    # Обучение модели
     history = model.fit(
         train_dataset.batch(batch_size),
         epochs=epochs,
@@ -23,23 +24,35 @@ def train_model(train_dataset, epochs=10, batch_size=32, learning_rate=0.001):
 
     return model, history
 
+
+def save_model(model: tf.keras.Model):
+    os.makedirs('saved_models', exist_ok=True)
+    os.makedirs('saved_models/' + str(date.today()), exist_ok=True)
+    path = 'saved_models/' + str(date.today()) + '/'
+
+    model.save_weights(path + 'weights_speech_intelligibility_assessment' + '.weights.h5')
+    model.save(path + 'model_speech_intelligibility_assessment' + '.keras')
+
+
+def save_history(history):
+    os.makedirs('saved_models', exist_ok=True)
+    os.makedirs('saved_models/' + str(date.today()), exist_ok=True)
+    path = 'saved_models/' + str(date.today()) + '/'
+    history_filename = path + 'history_speech_intelligibility_assessment' + '.pkl'
+    with open(history_filename, 'wb') as f:
+        pickle.dump(history.history, f)
+
+
 if __name__ == '__main__':
     train_dataset_path = 'dataset'
-    epochs = 500
+    epochs = 1000
     batch_size = 32
     learning_rate = 0.001
 
     dataset = tf.data.Dataset.load(train_dataset_path)
-    # dataset = dataset.shuffle(dataset.cardinality())
     train_dataset = dataset
-    print(train_dataset)
 
-    for samples, power in train_dataset.take(1):
-        print(f'Power: {power.numpy()}')
-        print(f'Samples: {samples.numpy()}')
-
-    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-    print(tf.test.is_built_with_cuda())
-
-    # with tf.device('/GPU:0'):
-    train_model(train_dataset, epochs=epochs, batch_size=batch_size, learning_rate=learning_rate)
+    model, history = train_model(train_dataset, epochs=epochs, batch_size=batch_size, learning_rate=learning_rate)
+    
+    save_model(model)
+    save_history(history)
